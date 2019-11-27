4ed3abd33e9288420ddf16df10b4d58ba84cb03f
Ahmad Wilson <awilson@nightcrawler.attlocal.net>
2019-11-27 10:54:31 -0500

    refactor and add integration tests for package installation and configuration

--- test/integration/vimrc-config/default_test.rb
+++ test/integration/vimrc-config/default_test.rb
@@ -1,10 +1,24 @@
-title "Default role integrated test file"
+title "vimrc configuration"
+
+describe file('/etc/vimrc') do
+  it { should exist }
+  its('mode') { should cmp '0664' }
+  its('owner') { should eq 'root' }
+  its('group') { should eq 'root' }
+
+  its('content') { should match("syntax on") }
+end
 
 describe file('/home/kitchen/.vimrc') do
   it { should exist }
   its('mode') { should cmp '0664' }
+  its('owner') { should eq 'kitchen' }
+  its('group') { should eq 'kitchen' }
 
-  its('content') { should match("set nocompatible") }
-  its('content') { should match("call vundle#begin") }
-  its('content') { should match("call vundle#end") }
+  its('content') { should match("set incsearch") }
+  its('content') { should match("set hlsearch") }
+  its('content') { should match("nnoremap j gj") }
+  its('content') { should match("nnoremap k gk") }
+  its('content') { should match("if exists") }
+  its('content') { should match("endif") }
 end
